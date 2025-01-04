7e5979c56bce682be86b3b7ab5d8f88a170d2e03
Divya <divyabaheti101@gmail.com>
2025-01-04 12:42:21 +0530

    footer stylings

--- src/components/VideoFooter.js
+++ src/components/VideoFooter.js
@@ -1,14 +1,22 @@
+import { MusicNote } from '@mui/icons-material';
 import './VideoFooter.css'
 import React from 'react';
 
 function VideoFooter({channel, description, song}){
     return (
         <div className='videoFooter'>
-            <h3>@{channel}</h3>
-            <p>{description}</p>
-            <p>{song}</p>
-            <img className='videoFooter_record' src='https://static.thenounproject.com/png/934821-200.png'
-            alt='' />
+            <div className='videoFooter_text'>
+                <h3>@{channel}</h3>
+                <p>{description}</p>
+                <div className='videoFooter_ticker'>
+                    <MusicNote className='videoFooter_icon'/>
+                    <p>{song}</p>
+                </div>
+                
+                <img className='videoFooter_record' src='https://static.thenounproject.com/png/934821-200.png'
+                alt='' />
+            </div>
+            
         </div>
     )
 }
